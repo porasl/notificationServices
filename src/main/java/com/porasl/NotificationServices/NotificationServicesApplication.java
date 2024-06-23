75bea41cf9a7e8d99951362d1df5624c1e95776a
hamid <hamid@porasl.com>
2024-06-23 00:45:02 -0700

    NotificationServices first checkIn

---
+++ src/main/java/com/porasl/NotificationServices/NotificationServicesApplication.java
@@ -0,0 +1,13 @@
+package com.porasl.NotificationServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porasl/NotificationServices/NotificationServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porasl.NotificationServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
