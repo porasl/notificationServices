7203dace837d8430c7882baad3dcc9b8d1c16568
hamid <hamid@porasl.com>
2024-06-23 00:59:23 -0700

    first draft

--- src/main/java/com/porasl/notificationServices/service/NotificationServicesApplication.java
+++ src/main/java/com/porasl/notificationServices/service/NotificationServicesApplication.java
@@ -1,4 +1,4 @@
-package com.porasl.NotificationServices;
+package com.porasl.notificationServices.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
