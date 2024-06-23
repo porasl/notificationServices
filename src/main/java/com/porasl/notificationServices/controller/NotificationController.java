8c7ecc3cc85a6415c0acc97d29af0da18dc3a733
porasl <151912720+porasl@users.noreply.github.com>
2024-06-23 01:12:18 -0700

    Update NotificationController.java

--- src/main/java/com/porasl/notificationServices/controller/NotificationController.java
+++ src/main/java/com/porasl/notificationServices/controller/NotificationController.java
@@ -1,5 +1,21 @@
 package com.porasl.notificationServices.controller;
 
+import com.example.model.NotificationMessage;
+import com.example.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class NotificationController {
 
+    @Autowired
+    private NotificationService notificationService;
+
+    @MessageMapping("/send")
+    @SendTo("/topic/notifications")
+    public NotificationMessage send(NotificationMessage message) {
+        return notificationService.sendNotification(message);
+    }
 }
