089f2f198399cb55bdb0094c99886f2c6f6063d0
hamid <hamid@porasl.com>
2024-06-23 01:39:06 -0700

    update of notificationServices

--- src/main/java/com/porasl/notificationServices/service/NotificationService.java
+++ src/main/java/com/porasl/notificationServices/service/NotificationService.java
@@ -1,12 +1,12 @@
 package com.porasl.notificationServices.service;
 
-import com.example.model.NotificationMessage;
+import com.porasl.notificationServices.model.NotificationMessage;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
 
     public NotificationMessage sendNotification(NotificationMessage message) {
-        return new NotificationMessage("Hello, " + message.getContent() + "!");
+        return new NotificationMessage("Message from porasl.com, " + message.getContent() + "!");
     }
 }
