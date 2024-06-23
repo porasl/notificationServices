9890cd3ee32325bc80f16c66a8470d7a0856d084
hamid <hamid@porasl.com>
2024-06-23 01:11:34 -0700

    Added Notification Messages and Controller

---
+++ src/main/java/com/porasl/notificationServices/controller/NotificationController.java
@@ -0,0 +1,5 @@
+package com.porasl.notificationServices.controller;
+
+public class NotificationController {
+
+}
---
+++ src/main/java/com/porasl/notificationServices/model/NotificationMessage.java
@@ -0,0 +1,5 @@
+package com.porasl.notificationServices.model;
+
+public class NotificationMessage {
+
+}
