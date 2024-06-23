9138da20f335bbbf9b1c111ae8a300137d2b29ad
porasl <151912720+porasl@users.noreply.github.com>
2024-06-23 01:12:56 -0700

    Update NotificationMessage.java

--- src/main/java/com/porasl/notificationServices/model/NotificationMessage.java
+++ src/main/java/com/porasl/notificationServices/model/NotificationMessage.java
@@ -1,5 +1,19 @@
 package com.porasl.notificationServices.model;
 
 public class NotificationMessage {
+    private String content;
 
+    public NotificationMessage() {}
+
+    public NotificationMessage(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
